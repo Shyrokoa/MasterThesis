901a94276be1f53a5981bfc29f15aea7054bc4e6
SHYROKOA <shirokoffandy@gmail.com>
2020-07-17 08:49:39 +0100

    finished cnn.py

--- root/code/cnn.py
+++ root/code/cnn.py
@@ -1,5 +1,12 @@
-import os, random
+import os
+import random
+import pickle
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 import cv2
+import numpy as np
 
 
 class CNN:
@@ -7,9 +14,18 @@ class CNN:
     IMG_SIZE = 25
 
     def __init__(self):
+        self.X = []
+        self.y = []
+        self.X_train = ''
+        self.X_test = ''
+        self.y_train = ''
+        self.y_test = ''
+        self.pickle_out = ''
+        self.model = ''
         self.training_data = []
-        self.create_training_data()
-        self.shuffle()
+
+        # evaluation
+        self.model_execution(.2, 64, 32, 3, 2, 10)
 
     # noinspection PyBroadException
     def create_training_data(self):
@@ -29,3 +45,104 @@ class CNN:
 
     def shuffle(self):
         random.shuffle(self.training_data)
+
+    def create_x_y(self):
+        for features, label in self.training_data:
+            self.X.append(features)
+            self.y.append(label)
+        self.X = np.array(self.X).reshape(-1, self.IMG_SIZE, self.IMG_SIZE, 1)
+
+    def object_serialization(self):
+        pickle_out = open('X.pickle', 'wb')
+        pickle.dump(self.X, pickle_out)
+        pickle_out.close()
+
+        pickle_out = open('y.pickle', 'wb')
+        pickle.dump(self.y, pickle_out)
+        pickle_out.close()
+
+    def load_objects(self):
+        pickle_in = open('X.pickle', 'rb')
+        self.X = pickle.load(pickle_in)
+
+        pickle_in = open('y.pickle', 'rb')
+        self.y = pickle.load(pickle_in)
+
+    def split_data(self, test_size):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X,
+                                                                                self.y,
+                                                                                test_size=test_size,
+                                                                                random_state=42)
+
+    def test_split_info(self):
+        print(f'X_train: {len(self.X_train)}/n'
+              f'X_test: {len(self.X_test)}/n'
+              f'y_train: {len(self.y_train)}/n'
+              f'y_test: {len(self.y_test)}')
+
+    def one_hot_encode(self):
+        # one-hot encode target column
+        self.y_train = to_categorical(self.y_train)
+        self.y_test = to_categorical(self.y_test)
+
+    def create_model(self, first_layer, second_layer, k_size, p_size):
+        self.model = Sequential()
+        self.model.add(Conv2D(first_layer,
+                              kernel_size=k_size,
+                              activation='relu',
+                              input_shape=(self.IMG_SIZE, self.IMG_SIZE, 1)))
+        self.model.add(MaxPooling2D(pool_size=(p_size, p_size)))
+        self.model.add(Conv2D(second_layer,
+                              kernel_size=k_size,
+                              activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(p_size, p_size)))
+        self.model.add(Flatten())
+        self.model.add(Dense(4, activation='softmax'))
+
+    def compile_model(self):
+        # compile model using accuracy to measure model performance
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def fit_model(self, epochs):
+        # train the model
+        self.model.fit(self.X_train,
+                       self.y_train,
+                       validation_data=(self.X_test, self.y_test),
+                       epochs=epochs)
+
+    def check(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        dim = (self.IMG_SIZE, self.IMG_SIZE)
+        # resize image
+        resized = cv2.resize(gray, dim, interpolation=cv2.INTER_AREA)
+        # resized = cv2.bitwise_not(resized)
+        ret, resized = cv2.threshold(resized, 20, 255, cv2.THRESH_BINARY)
+        a = resized
+        a = np.expand_dims(a, axis=(0, 3))
+        arr = self.model.predict(a)
+        arr = arr.tolist()[0]
+        if arr.index(max(arr)) == 0:
+            return 'circle'
+        elif arr.index(max(arr)) == 1:
+            return 'square'
+        elif arr.index(max(arr)) == 2:
+            return 'star'
+        elif arr.index(max(arr)) == 3:
+            return 'triangle'
+        else:
+            return 'Error!'
+
+    def model_execution(self, test_size,first_layer, second_layer, k_size, p_size, epochs):
+        self.create_training_data()
+        self.shuffle()
+        self.create_x_y()
+        self.object_serialization()
+        self.load_objects()
+        self.split_data(test_size)
+        self.one_hot_encode()
+        self.create_model(first_layer, second_layer, k_size, p_size)
+        self.compile_model()
+        self.fit_model(epochs)
+
+    def model_save(self):
+        self.model.save_weights("model.h5")
--- root/code/test.py
+++ root/code/test.py
@@ -1,6 +1,4 @@
 from root.code.cnn import CNN
 
 cnn = CNN()
-cnn.shuffle()
-for sample in cnn.training_data:
-    print(sample[1])
+
